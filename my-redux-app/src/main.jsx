ee4fd5b41871b7dfc9b2eff3e99681a6b351457a
goutamgdy <goutamgdy@gmail.com>
2025-10-26 20:23:00 +0530

    Workflows Added

--- my-redux-app/src/main.jsx
+++ my-redux-app/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { provider } from "react-redux";
+import { store } from './redux/store.js';
+
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOm.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <provider>
+      <App />
+    </provider>
+  </React.StrictMode>,
 )
