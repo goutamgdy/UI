da2b1ef35c34b63f575bcbab9b1b223d8736a881
goutamgdy <goutamgdy@gmail.com>
2025-10-28 01:15:42 +0530

    react

---
+++ react/app1/src/components/Kiya.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Kiya(){
+    return (
+        <div>
+            <h2>
+                I am here...
+            </h2>
+        </div>
+    );
+}
+
+export default Kiya
